68a6e55baf74c3dd441edd261ec6f482a9e3bc55
Yashvi Patel <159598007+yashviii27@users.noreply.github.com>
2025-12-10 17:57:02 +0530

    Final Updated code

--- src/modules/groups/groups.service.ts
+++ src/modules/groups/groups.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException, BadRequestException } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { PrismaService } from "../../common/prisma.service";
 import { CreateGroupDto } from "./dto/create-group.dto";
 import { UpdateGroupDto } from "./dto/update-group.dto";
