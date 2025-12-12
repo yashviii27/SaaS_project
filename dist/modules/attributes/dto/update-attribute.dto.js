62ab88118eaae704d25f06e5bac1f620ef115c52
Yashvi Patel <159598007+yashviii27@users.noreply.github.com>
2025-12-12 14:23:00 +0530

    Remove node_modules and dist from repo

--- dist/modules/attributes/dto/update-attribute.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateAttributeDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_attribute_dto_1 = require("./create-attribute.dto");
-class UpdateAttributeDto extends (0, mapped_types_1.PartialType)(create_attribute_dto_1.CreateAttributeDto) {
-}
-exports.UpdateAttributeDto = UpdateAttributeDto;
-//# sourceMappingURL=update-attribute.dto.js.map
--- dist/modules/generics/dto/update-generic.dto.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.UpdateGenericDto = void 0;
-const mapped_types_1 = require("@nestjs/mapped-types");
-const create_generic_dto_1 = require("./create-generic.dto");
-class UpdateGenericDto extends (0, mapped_types_1.PartialType)(create_generic_dto_1.CreateGenericDto) {
-}
-exports.UpdateGenericDto = UpdateGenericDto;
-//# sourceMappingURL=update-generic.dto.js.map
